5d152e0b80f6c071c608928afd4882ea1404fd7d
Eyal Green <166248952+ech0zdev@users.noreply.github.com>
2024-04-21 13:28:07 +0300

    Added code files

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center"><b>Welcome to Ech0z Cfx IP </b><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35"></h1>
+
+---
+
+<h1 align="center"><b>Ech0z Cfx-To-Ip is the easiest tool to get any cfx server IP from URL like: cfx.re/join/kx5la6</h1>
+
+---
+
+<h1 align="center"><b>Preview</h1>
+    <br/>
+![Preview](https://media.discordapp.net/attachments/1230099510957572106/1231549703779385394/image.png?ex=66375d0a&is=6624e80a&hm=aba05497d6c79c4725806c3bbaca7efea2d127d3b90ee6c0dc54ac060aad7cec&=&format=webp&quality=lossless&width=781&height=184)
+
+---
+<h1 align="center"><b>Packages Used</h1>
+    <br/>
+- [Axios@1.6.8](https://www.npmjs.com/package/axios/v/1.6.8)
+- [Chalk@4.1.2](https://www.npmjs.com/package/chalk/v/4.1.2)
+- [Readline@1.3.0](https://www.npmjs.com/package/readline/v/1.3.0)
+
+---
+
+<h1 align="center"><b>Credits</h1>
+    <br/>
+
+Made by [ech0z.dev](https://discord.com/users/1135627677441736704)
---
+++ index.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const readline = require('readline');
+const chalk = require('chalk');
+const { promisify } = require('util');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const ques = promisify(rl.question).bind(rl);
+
+const resolve = async (url) => {
+    try {
+        const response = await axios.get(url, {
+            validateStatus: (status) => status >= 200 && status < 300
+        });
+
+        return response.headers["x-citizenfx-url"].replace("http://", "").replace("/", "");
+    } catch (e) {
+        throw new Error(e.message);
+    }
+};
+
+const ask = async () => {
+    const input = await ques(chalk.green.bold('Please your CFX URL: '));
+
+    let format = input.trim();
+    if (!format.startsWith('http')) format = `https://${format}`;
+
+    try {
+        const ip = await resolve(format);
+        console.log(chalk.yellow.bold(`${format} => ${ip}`));
+    } catch (e) {
+        console.log(chalk.red.bold(e.message));
+    } finally {
+        rl.close();
+    }
+};
+
+const run = async () => {
+    try {
+        await ask();
+    } catch (e) {
+        console.log(chalk.red.bold(e.message));
+    }
+};
+
+run();
